43cdd5c4a8632acf0cc3099541b92643f58ca5b3
“viktorcvp” <dffggg2013@yandex.by>
2022-10-12 20:14:39 +0300

    created a c# file and wrote a program according to the algorithm

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите первое число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a > b)
+{
+    Console.WriteLine(a + "больше" + b);
+}
+
+else Console.WriteLine("число" + b + "больше" + a);
+
